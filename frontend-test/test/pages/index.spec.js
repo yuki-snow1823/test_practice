27870d3dc95a6eac9d1e8bef6f7fbab8f215a685
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-22 10:50:35 +0900

    [add]試行錯誤     TypeError: Cannot read property 'state' of undefined が解決できない

--- frontend-test/test/pages/index.spec.js
+++ frontend-test/test/pages/index.spec.js
@@ -4,18 +4,25 @@ import index from "@/pages/index.vue";
 import { cloneDeep } from "lodash";
 import indexStore from "@/store/index.js"
 
-const localVue = createLocalVue();
-localVue.use(Vuex);
+// const localVue = createLocalVue();
+// localVue.use(Vuex);
+
 
 describe("index.vueのテスト", () => {
   let store;
   beforeEach(() => {
     // const wrapper = mount(index);
-    store = new Vuex.Store(cloneDeep(indexStore));
+    // store = new Vuex.Store(cloneDeep(indexStore));
   });
   test("タイトルが表示されている", () => {
     // console.log(wrapper);
-    const wrapper = mount(index);
+    const wrapper = mount(index, {
+      store
+    });
+    store = new Vuex.Store({
+      state: {},
+    });
+    expect(store.state.count).toBe(0);
     expect(wrapper.find(".title")).toBeTruthy();
   });
   test("カウント前の表示が0", () => {
