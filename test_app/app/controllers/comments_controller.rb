7dd1fb9a5ac015747cd0e30dd673a3a14e238060
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-23 20:58:22 +0900

    [add]フラッシュメッセージの設定

--- test_app/app/controllers/comments_controller.rb
+++ test_app/app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  def create
+    comment = Comment.new(comment_params)
+    comment.user_id = current_user.id
+    if comment.save
+     flash[:success] = "comment was successfully created."
+    end
+    redirect_to users_path
+  #  else
+  #   @books = Book.all
+  #   @user = current_user
+  #   render action: :index
+  end
+  
+
+  private
+  def comment_params
+    params.require(:comment).permit(:context)
+  end
+
 end
