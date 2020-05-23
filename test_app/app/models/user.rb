9be748673f1798c271423c9903eddb4d2a4c6d84
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-23 21:52:22 +0900

    [add]メールのバリデーション

--- test_app/app/models/user.rb
+++ test_app/app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :comments, dependent: :destroy
+  validates :email, presence: true
 end
