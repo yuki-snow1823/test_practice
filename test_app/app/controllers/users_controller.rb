c1b586bf583f99864614286dfc4d4e3520181ba6
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-23 20:58:54 +0900

    [add]コメントが表示されるように修正

--- test_app/app/controllers/users_controller.rb
+++ test_app/app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
+    # comments = user.comments.all
+    @comment = Comment.new
+    @comments = current_user.comments
   end
 end
