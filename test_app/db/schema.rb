47dce2899a630b3cf6e0f54ada7240d087598658
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-23 20:59:37 +0900

    [add]リレーションの修正、テストの準備完了

--- test_app/db/schema.rb
+++ test_app/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_211224) do
+ActiveRecord::Schema.define(version: 2020_05_23_114107) do
 
   create_table "comments", force: :cascade do |t|
     t.text "context"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
