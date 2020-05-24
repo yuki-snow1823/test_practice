974d46035a9944f9bea3188749a2246b56785a9f
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-24 09:03:29 +0900

    [add]試行錯誤

--- frontend-test/test/pages/index.spec.js
+++ frontend-test/test/pages/index.spec.js
@@ -2,29 +2,25 @@ import { mount, createLocalVue } from "@vue/test-utils";
 import Vuex from "vuex";
 import index from "@/pages/index.vue";
 import { cloneDeep } from "lodash";
-import indexStore from "@/store/index.js"
+import indexStore from "@/store/index.js";
+import * as store from "@/store";
 
-// const localVue = createLocalVue();
-// localVue.use(Vuex);
-
+const localVue = createLocalVue();
+localVue.use(Vuex);
 
 describe("index.vueのテスト", () => {
   let store;
   beforeEach(() => {
     // const wrapper = mount(index);
     // store = new Vuex.Store(cloneDeep(indexStore));
+    store = new Vuex.Store({
+      state: {}
+    });
   });
   test("タイトルが表示されている", () => {
     // console.log(wrapper);
     const wrapper = mount(index, {
-      methods: {
-        doAction: function() {
-          this.$store.state.counter++;
-        }
-      }
-    });
-    store = new Vuex.Store({
-      state: {},
+      store
     });
     expect(store.state.count).toBe(0);
     expect(wrapper.find(".title")).toBeTruthy();
