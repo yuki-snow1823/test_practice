cb1f79bbc06c1f4a4308dffc177ed0a67bd49f00
yuki-snow1823 <magic.iroha@gmail.com>
2020-05-25 18:41:23 +0900

    [add]comment　model テストの修正

--- test_app/spec/models/comment_spec.rb
+++ test_app/spec/models/comment_spec.rb
@@ -1,5 +1,13 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Comment, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Comment, type: :model do
+  before(:each) do
+    @comment = create(:comment)
+  end
+
+  context "バリデーションについて" do
+    it "通常時にコメントが保存されるか" do
+      expect(@comment).to be_valid
+    end
+  end
+end
